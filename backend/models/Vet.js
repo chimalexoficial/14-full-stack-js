31ceef3f2f6a1ccc991efb8ac797edb6b75529b5
Alexis Salgado <chimalexoficial@gmail.com>
2024-05-13 08:54:19 -0600

    Adding vet schema

---
+++ backend/models/Vet.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const vetSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        default: null,
+        trim: true
+    },
+    web: {
+        type: String,
+        default: null
+    },
+    token: {
+        type: String
+    },
+    confirmed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Vet = mongoose.model('vet', vetSchema);
+export default Vet;
