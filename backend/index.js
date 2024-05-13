134a37149159487b394bf8a305a0981d73d3b7a2
Alexis Salgado <chimalexoficial@gmail.com>
2024-05-13 00:34:11 -0600

    server express port 4000 backend

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,12 @@
 import express from 'express';
 
+const app = express();
+const PORT = 4000;
+
+app.use('/', (req, res) => {
+    res.send('Hello');
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
